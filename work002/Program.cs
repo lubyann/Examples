5c1e9d23e287ae71172584488ab063fa6c314959
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-02-10 11:23:58 +0300

    add work002 min/max

---
+++ work002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter a number1: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Enter a number2: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+if(numberA > numberB) 
+{
+    Console.WriteLine("Max = "+numberA);
+    Console.WriteLine("Min = "+numberB);
+}
+else
+{
+    Console.WriteLine("Max = "+numberB);
+    Console.WriteLine("Min = "+numberA);
+}
