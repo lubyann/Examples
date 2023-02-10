3518a27cbf4a052e5b8ba4f470480fcfa0e1c012
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-02-11 02:06:47 +0300

    add dogs friends

--- dogs_friends/Program.cs
+++ dogs_friends/Program.cs
@@ -1,28 +1,26 @@
-﻿int count = 0;
-int distance = 10000;
-int first_friend_speed = 2;
+﻿int first_friend_speed = 2;
 int second_friend_speed = 1;
 int dogspeed = 5;
-int friend = 2;
+int distance = 10000;
 int time = 0;
+int friend = 2;
 
-while(distance < 10)
+int count = 0;
+while (distance > 10)
 {
-    
-    if(friend == 1)
+    if (friend == 1)
     {
         time = distance / (dogspeed + first_friend_speed);
         friend = 2;
-    
-    }    
+
+    }
     else
     {
-         time = distance / (dogspeed - second_friend_speed);
-         friend = 1;
-         
-    }
-    distance = distance / (first_friend_speed - second_friend_speed) * time;
-    count = count + 1;
+        time = distance / (dogspeed - second_friend_speed);
+        friend = 1;
 
-Console.Write(count);
+    }
+    distance = distance - (first_friend_speed - second_friend_speed) * time;
+    count++;
 }
+Console.Write(count);
