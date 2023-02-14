844a0ed35c98ec42134e8f5653a35abdbfd1d6f9
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-02-14 12:27:17 +0300

    add task11

---
+++ Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// 11 Напишите программу, которая выводит случайное 
+// трезначное число и удаляет вторую цифру этого числа.
+
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+// int a1 = num / 100;
+// int a2 = num % 10;
+
+// Console.WriteLine(num);
+// Console.WriteLine($"{a1}" + $"{a2}");
+
+int result = (num / 100) * 10 + num % 10;
+Console.WriteLine($"{num} -> {result}");
+Console.WriteLine($"{num} -> {num / 100}{num % 10}");
+
